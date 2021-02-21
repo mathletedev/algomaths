f94c28572357760ad6b8b56a124906317cb49d30
MathleteDev <mathletedev@gmail.com>
2021-02-21 10:34:40 -0800

    Pascal's Triangle efficiency

--- scripts/pascals_triangle/increment.py
+++ scripts/pascals_triangle/increment.py
@@ -2,10 +2,10 @@ def increment(index, row=[1], depth=0):
   if depth == index:
     return row
 
-  tmp = []
-  zeros = [0] + row + [0]
-  for i in range(len(zeros) - 1):
-    tmp.append(zeros[i] + zeros[i + 1])
+  tmp = [1]
+  for i in range(len(row) - 1):
+    tmp.append(row[i] + row[i + 1])
+
+  tmp.append(1)
 
   return increment(index, tmp, depth + 1)
-  
